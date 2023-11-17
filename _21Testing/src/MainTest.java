5e2560503b80413e6fe2e60162536adf214a5aad
ManuelBasso <manuel.basso.89@gmail.com>
2023-11-17 11:20:56 +0100

    update exercise

--- _21Testing/src/MainTest.java
+++ _21Testing/src/MainTest.java
@@ -8,20 +8,26 @@ class MainTest {
     private final Main testing = new Main();
 
     @Test
-    void stringLength() {
+    void stringLength_withString() {
         Integer result = testing.stringLength("Ciao");
-        assertEquals(4,result,"result = 4");
+        assertEquals(4, result, "result = 4");
     }
 
     @Test
-    void stringLengthZero(){
+    void stringLength_with_char_and_numbers() {
+        Integer result = testing.stringLength("erj8738oej**");
+        assertEquals(12, result, "result = 4");
+    }
+
+    @Test
+    void stringLengthZero() {
         Integer result = testing.stringLength("");
-        assertEquals(0,result,"Empty string, result = 0");
+        assertEquals(0, result, "Empty string, result = 0");
     }
 
     @Test
-    void stringLengthNull(){
+    void stringLengthNull() {
         Integer result = testing.stringLength(null);
-        assertEquals(null, result, "string value null, result = null");
+        assertNull(result, "string value null, result = null");
     }
 }
